bedc8a7eba38b55ecf61239f5e1cc493881b6964
zhanls <zhanleshun@163.com>
2023-06-03 14:08:16 +0800

    Initial Commit

---
+++ MobileStudio/MobileStudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileStudioApp.swift
+//  MobileStudio
+//
+//  Created by 詹乐顺 on 2023/6/3.
+//
+
+import SwiftUI
+
+@main
+struct MobileStudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
